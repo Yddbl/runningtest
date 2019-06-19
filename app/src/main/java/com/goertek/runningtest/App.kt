e693e8b187bf577fb603116ee1128046293f3e78
铁血男儿王钢蛋 <425369969@qq.com>
2019-06-19 14:35:45 +0800

    application and gradle

---
+++ app/src/main/java/com/goertek/runningtest/App.kt
@@ -0,0 +1,20 @@
+package com.goertek.runningtest
+
+import android.app.Application
+import android.content.Context
+import android.util.Log
+
+class App : Application() {
+
+    private val TAG = "App";
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG,"onCreate");
+        context = this;
+    }
+
+    companion object {
+        lateinit var context: Context;
+    }
+}
